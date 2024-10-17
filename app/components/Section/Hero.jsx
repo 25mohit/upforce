6c0704effa41cfe6a4ee8e15bec4fe1ab8fc0c85
25mohit <mohit724agarwal@gmail.com>
2024-10-17 23:16:04 +0530

    C

--- app/components/Section/Hero.jsx
+++ app/components/Section/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
           <h2 className='text-2xl'>Event Management</h2>
         </div>
         <p>From Concept to Celebration – We Make Every Moment Unforgettable</p>
-        <p>Whether it's a corporate event, wedding, or special occasion, we bring your vision to life with flawless execution and attention to detail. Let us handle the planning, so you can enjoy the memories.</p>
+        <p>Whether it&apos;s a corporate event, wedding, or special occasion, we bring your vision to life with flawless execution and attention to detail. Let us handle the planning, so you can enjoy the memories.</p>
         <i>Expert planning. Stunning designs. Seamless experiences.</i>
       </div>
       <div className='w-full md:w-1/2 flex gap-4 justify-center flex-col'>
