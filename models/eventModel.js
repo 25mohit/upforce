bd4ff4cf385b536179703775597b6aebf607ce6b
25mohit <mohit724agarwal@gmail.com>
2024-10-19 15:15:30 +0530

    Descriptino added in Model

--- models/eventModel.js
+++ models/eventModel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const eventSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
   name: { type: String, required: true },
+  description: { type: String, required: true },
   date: { type: Date, required: true },
   status: { type: String, required: true },
 }, { timestamps: true });
